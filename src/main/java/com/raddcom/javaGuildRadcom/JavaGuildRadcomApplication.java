23d7c431be6ea5ed9afcb3bb6a5cf60ae9f56197
Slava Zavgorodniy <slava.zavgorodniy@radcom.com>
2022-08-28 06:05:01 +0100

    initial commit

---
+++ src/main/java/com/raddcom/javaGuildRadcom/JavaGuildRadcomApplication.java
@@ -0,0 +1,13 @@
+package com.raddcom.javaGuildRadcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGuildRadcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGuildRadcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raddcom/javaGuildRadcom/JavaGuildRadcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raddcom.javaGuildRadcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGuildRadcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
